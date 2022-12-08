642b0605134b896f206d0beae1683663b3311977
LuisParedes <luisparedes1009@hotmail.com>
2022-12-08 18:20:00 -0500

    in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import contacts from "./contacts.json";
 
 function App() {
+  let data = [...contacts];
+  let fiveContacts = data.slice(0, 5);
+  let newArray = data.slice(5);
+  const [celebs, setCelebs] = useState(fiveContacts);
+  
+
+
+  function addRandom() {
+    let randomContact = newArray[Math.floor(Math.random() * newArray.length)];
+    setCelebs([...celebs, randomContact]);
+  }
+
+ function sortPopularity (){
+    let populatirySorted = [...celebs].sort((a, b) =>
+    console.log('hello')
+     
+    );
+    setCelebs(populatirySorted);
+ }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      <div>
+        <button onClick={addRandom}>Add Random Contact</button>
+        <button onClick={sortPopularity}>>Sort by Populatiry</button>
+        <button>Sort by Name</button>
+      
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Populatiry</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {celebs.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt="" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              {contact.wonOscar ? <td>🏆</td> : <td></td>}
+              {contact.wonEmmy ? <td>🏆</td> : <td></td>}
+              <td>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
